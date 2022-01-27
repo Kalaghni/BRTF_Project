6a35cbce1846b3fcde5b307a26187e4acecfdcbe
Zachpbabin1 <73443030+Zachpbabin1@users.noreply.github.com>
2022-01-27 17:58:50 -0500

    Added some utility class's

---
+++ BRFT_Booking/Utilities/CookieHelper.cs
@@ -0,0 +1,30 @@
+﻿using Microsoft.AspNetCore.Http;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace BRFT_Booking.Utilities
+{
+    public static class CookieHelper
+    {
+        /// <summary>  
+        /// set the cookie  
+        /// </summary>  
+        /// <param name="_context">the HttpContext</param>  
+        /// <param name="key">key (unique indentifier)</param>  
+        /// <param name="value">value to store in cookie object</param>  
+        /// <param name="expireTime">expiration time</param>  
+        public static void CookieSet(HttpContext _context, string key, string value, int? expireTime)
+        {
+            CookieOptions option = new CookieOptions();
+
+            if (expireTime.HasValue)
+                option.Expires = DateTime.Now.AddMinutes(expireTime.Value);
+            else
+                option.Expires = DateTime.Now.AddMilliseconds(10);
+
+            _context.Response.Cookies.Append(key, value, option);
+        }
+    }
+}
